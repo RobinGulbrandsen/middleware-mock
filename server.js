1ee6c3ad2cfb9826be564a125fe34f74a3746925
Robin Gulbrandsen <robin@123.no>
2017-11-28 11:40:17 +0000

    init

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const express = require('express');
+const middleware = require('./middleware');
+
+let server = null;
+
+module.exports.start = (port) => {
+	const app = express();
+	app.set('port', port);
+
+	app.get('/', middleware.setUser, (req, res) => {
+		res.send();
+	});
+
+	server = http.createServer(app).listen(app.get('port'));
+	return server;
+};
+
+module.exports.close = () => {
+	if (server && server.close) server.close();
+};
---
+++ server_test.js
@@ -0,0 +1,43 @@
+const supertest = require('supertest');
+const {expect} = require('chai');
+const sinon = require('sinon');
+
+describe('server', () => {
+
+	let middleware;
+	let server;
+
+	beforeEach(() => {
+		middleware = require('./middleware');
+        sinon.stub(middleware, 'setUser').callsFake(function(req, res, next) {
+
+        	//Fake middleware to set a token for test purpose
+			res.set('Authorization', 'Bearer TEST');
+			req.user = {
+				username: 'test',
+				roles: ['admin', 'member', 'test']
+			};
+
+            return next();
+        });
+        server = require('./server');
+    });
+
+    afterEach(function() {
+        middleware.setUser.restore();
+    });
+
+    it('should set test user in middleware', (done) => {
+    	const serverInstance = server.start(1332);
+    	supertest(serverInstance)
+    		.get('/')
+    		.expect('Authorization', 'Bearer TEST')
+    		.expect(200)
+    		.end(err => {
+    			server.close();
+    			if (err) throw err;
+    			done();
+    		});
+    });
+
+});
